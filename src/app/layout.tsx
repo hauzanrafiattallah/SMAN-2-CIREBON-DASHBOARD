4d4cea67a5f67f95d0eb6c2e9126c94dd07c5848
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-01 14:22:31 +0700

    Update title in layout.tsx for conciseness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SMAN 2 Cirebon School Management Dashboard",
+  title: "SMAN 2 Cirebon Dashboard",
   description: "School Management System",
 };
 
