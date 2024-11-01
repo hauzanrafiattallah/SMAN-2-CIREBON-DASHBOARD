bfafab8aad64477577f41cf7c464e17f105e5e1d
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-01 14:10:42 +0700

    Update metadata description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SMAN 2 Cirebon School Management Dashboard",
-  description: "Next.js School Management System",
+  description: "School Management System",
 };
 
 export default function RootLayout({
