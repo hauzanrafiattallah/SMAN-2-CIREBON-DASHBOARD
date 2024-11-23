4399191e469f0bcc79dc0df4c3cacabd2ecf496a
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-24 01:35:34 +0700

    Add BigCalendar and integrate with StudentPage; enhance menu visibility based on user role

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,7 +1,25 @@
+import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col xl:flex-row ">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="rounded-md h-full bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule Hauzan Ganteng</h1>
+          <BigCalendar />
+        </div>
+      </div>
 
-export default StudentPage
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default StudentPage;
