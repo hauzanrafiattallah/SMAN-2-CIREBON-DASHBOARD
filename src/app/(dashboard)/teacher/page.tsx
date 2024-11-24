5cc9a2823ce27439b2be5bb86d2757b31588f887
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-24 10:45:58 +0700

    Integrate BigCalendar and Announcements components into TeacherPage; enhance layout structure and update event dates

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,24 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
+
 const TeacherPage = () => {
   return (
-    <div className=''>TeacherPage</div>
-  )
-}
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row ">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="rounded-md h-full bg-white p-4">
+          <h1 className="text-xl font-semibold">Schedule Guru</h1>
+          <BigCalendar />
+        </div>
+      </div>
 
-export default TeacherPage
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default TeacherPage;
