df47d99087aa16ed9615841c0006db0833138889
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-05 22:14:22 +0700

    SMACD-5 || Add TeacherForm component for create and update functionality || HauzanDev

---
+++ src/components/forms/TeacherForm.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const TeacherForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
+}) => {
+  return <form className="">Input</form>;
+};
+
+export default TeacherForm;
