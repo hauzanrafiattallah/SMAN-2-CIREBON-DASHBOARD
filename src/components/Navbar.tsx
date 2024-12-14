35f2e27101b315f84af832de8c64d8c91c1dbace
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-14 16:31:51 +0700

    SMACD-33 || Refactor authentication and homepage components; implement Clerk for user management and update layout structure || HauzanDev

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 const Navbar = () => {
   return (
@@ -5,7 +6,11 @@ const Navbar = () => {
       {/* search bar */}
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="" width={14} height={14} />
-        <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] p-2 bg-transparent outline-none"
+        />
       </div>
       {/* icons and user */}
       <div className="flex items-center gap-6 justify-end w-full">
@@ -22,13 +27,14 @@ const Navbar = () => {
           <span className="text-xs leading-3 font-medium">Hauzan Ganteng</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
+        {/* <Image
           src="/avatar.png"
           alt=""
           width={36}
           height={36}
           className="rounded-full"
-        />
+        /> */}
+        <UserButton />
       </div>
     </div>
   );
