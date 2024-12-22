c67b4ea088231156a7e3f99a00677856fa91bb5c
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-12-22 22:55:03 +0700

    SMACD-36 || Refactor UserCard component to support dynamic user type counts and update AdminPage to display admin and student cards || HauzanDev

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,20 @@
+import prisma from "@/lib/prisma";
 import Image from "next/image";
 
-const UserCard = ({ type }: { type: string }) => {
+const UserCard = async ({
+  type,
+}: {
+  type: "admin" | "teacher" | "student" | "parent";
+}) => {
+  const modelMap: Record<typeof type, any> = {
+    admin: prisma.admin,
+    teacher: prisma.teacher,
+    student: prisma.student,
+    parent: prisma.parent,
+  };
+
+  const data = await modelMap[type].count();
+
   return (
     <div className="rounded-2xl odd:bg-Purple even:bg-Yellow p-4 flex-1">
       <div className="flex justify-between items-center">
@@ -15,7 +29,7 @@ const UserCard = ({ type }: { type: string }) => {
           height={20}
         />
       </div>
-      <h1 className="text-2xl font-semibold my-4">1,234</h1>
+      <h1 className="text-2xl font-semibold my-4">{data}</h1>
       <h2 className="capitalize text-sm font-medium text-gray-500">{type}</h2>
     </div>
   );
